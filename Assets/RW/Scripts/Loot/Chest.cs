f214ec70069cc4a604f842114ee901b8481f2ba7
GitTuNguyen <tunguyen.96lol@gmail.com>
2023-04-08 23:31:05 +0700

    Add new Loot item : Chest

---
+++ Assets/RW/Scripts/Loot/Chest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PickUpArea"))
+        {
+            if (player == null)
+            {
+                player = FindObjectOfType<Player>();
+            }
+            AudioManager.Instance.PlaySFX("PickUpChicken");
+            player.PickUpChest();
+            Destroy(gameObject);
+        }
+    }
+}
